6783b34e9b2d3bde39cc904f2c078c26a275ad35
Isaac Faulkner <isaacfaulkner1@gmail.com>
2023-05-08 13:57:36 -0500

    made it so generateDiv now creates a p tag the has the value of clickCount and click count now only counts up and only up.

--- scripts/script.js
+++ scripts/script.js
@@ -7,16 +7,19 @@ function onReady() {
         $('.genBtn').on('click', genDiv);
         $('.divSection').on('click', '.yellow', colorSwitch);
         $('.divSection').on('click', '.del', removeDiv);
-        displayCount();
+        // displayCount();
 }
+//counts the amount of buttons generated.
+let clickCount = 1;
 
-let clickCount = 0;
-
+//when called generates new div with 2 buttons as children.
+// then adds 1 to click count and calls displayCount
 function genDiv() {
 
         $('.divSection').append(`
         
-            <div class="child">hello
+            <div class="child">
+            <p>${clickCount}</p>
 
             <button class="yellow">Yellow</button>
 
@@ -26,32 +29,33 @@ function genDiv() {
         
         `);
 clickCount += 1;
-displayCount();
+// displayCount();
 
 
 
 }
+//changes the text displayed on the dom based on the value of clickCount.
+// function displayCount() {
 
-function displayCount() {
-
-        $('#counter').text(clickCount);
+//         $('#counter').text(clickCount);
 
 
 
-}
-
+// }
+//switches the background color to yellow.
 function colorSwitch() {
         // console.log('hello'); 
         $(this).parent().css({backgroundColor: "yellow"})
 
 
 }
-
+//removes the parent of this when clicked.
+//subtracts on form the clickCount and calls the displayCount.
 function removeDiv() {
     // console.log('hello2');
     $(this).parent().remove();
 
-    clickCount -= 1;
-    displayCount();
+    // clickCount -= 1;
+    // displayCount();
 
 }
